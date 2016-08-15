10bbe047c9407cc1a3663aec076c80d7b37a8638
razwanizmi <razwan.izmi@gmail.com>
2016-08-15 14:29:19 +0800

    Modify readme

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,22 @@ require_relative '../app/models/url'
 
 class UrlImporter
   def self.import(filename)
-    csv = File.open(filename)
-    csv.each_line do |row|
-      Url.create(long_url: row[1..-4])
+    file = File.open(filename)
+    Url.transaction do
+      file.each_line do |row|
+        Url.connection.execute "INSERT INTO urls (long_url) VALUES ('#{row[1..-4]}')"
+      end
     end
   end
 end
+
+# class UrlImporter
+#   def self.import(filename)
+#     csv = File.open(filename)
+#     csv.each_line do |row|
+#       Url.create(long_url: row[1..-4])
+#     end
+#   end
+# end
 
 UrlImporter.import('db/data/urls.txt')
